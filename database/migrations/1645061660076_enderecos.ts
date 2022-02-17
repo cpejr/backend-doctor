e8f92ce5cbd4190b2773933f1b5789b76d676d33
Maria <mariamsilva15@ufmg.br>
2022-02-16 23:09:20 -0300

    feat: migration de endereços

---
+++ database/migrations/1645061660076_enderecos.ts
@@ -0,0 +1,30 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Enderecos extends BaseSchema {
+  protected tableName = 'enderecos'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').notNullable()
+      table.string('cep', 8).notNullable()
+      table.string('pais').notNullable()
+      table.string('estado', 2).notNullable()
+      table.string('cidade').notNullable()
+      table.string('bairro').notNullable()
+      table.string('rua').notNullable()
+      table.integer('numero').notNullable()
+      table.string('complemento').nullable()
+
+      
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
