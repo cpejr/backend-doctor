2c0e7d83d7ac64b447dd60fdacb554475e0a3e6d
Maria <mariamsilva15@ufmg.br>
2022-02-18 23:15:54 -0300

    feat: migration consultas

---
+++ database/migrations/1645236650829_dispositivos.ts
@@ -0,0 +1,23 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Dispositivos extends BaseSchema {
+  protected tableName = 'dispositivos'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('titulo').notNullable()
+      table.boolean('esta_disponivel').notNullable()
+      
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
