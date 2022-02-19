66cdacf912cc6ba2ddaac291d962b004e118577a
Maria <mariamsilva15@ufmg.br>
2022-02-18 23:21:17 -0300

    feat: migration exame_marcados

---
+++ database/migrations/1645237008523_exame_marcados.ts
@@ -0,0 +1,33 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class ExameMarcados extends BaseSchema {
+  protected tableName = 'exame_marcados'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.time('horario').notNullable()
+      table.date('data').notNullable()
+      table.string('descricao').nullable()
+      table.date('data_envio').notNullable()
+      table.date('data_devolucao').notNullable()
+      table.date('data_pagamento').notNullable()
+      table.boolean('esta_atrasado').notNullable()
+      table.boolean('esta_disponivel').notNullable()
+      table.integer('id_usuario').unsigned().references('id').inTable('usuarios');
+      table.integer('id_exame').unsigned().references('id').inTable('exames');
+      table.integer('id_consultorio').unsigned().references('id').inTable('consultorios');
+      table.integer('id_dispositivo').unsigned().references('id').inTable('dispositivos');
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
