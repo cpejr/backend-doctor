9ce55660154baea9bc9cb4947f930b6798d5b5a2
Maria <mariamsilva15@ufmg.br>
2022-02-22 19:35:06 -0300

    feat: finalizou controller + rota + model endereco

--- app/Repositories/EnderecosRepository.ts
+++ app/Repositories/EnderecosRepository.ts
@@ -4,10 +4,14 @@ import EnderecosDTO from 'App/DTO/EnderecosDTO'
 export default class EnderecosRepository {
   public static async find(params: EnderecosDTO) {
     const chaves = Object.keys(params)
-    // let parametrosValidos = chaves.forEach(() => {
-
-    // })
-    const result = await Database.query().from('enderecos').where(params)
+    let parametrosValidos = []
+    chaves.forEach((chave) => {
+      if (params[chave]) {
+        parametrosValidos[chave] = params[chave]
+      }
+      
+    })
+    const result = await Database.query().from('enderecos').where(parametrosValidos)
     return result
   }
 }
