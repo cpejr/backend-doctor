3dcf04d3b7aa56f328151fe79fffe2ada56f8703
Mateus Pardini <mateuspardini6@gmail.com>
2022-03-04 19:37:32 -0300

    fix: Arrumando as váriaveis JSON dos validators

--- app/Validators/FormularioValidator.ts
+++ app/Validators/FormularioValidator.ts
@@ -1,30 +1,38 @@
 import { schema, rules } from '@ioc:Adonis/Core/Validator'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-export default class FormularioValidator {
+export class FormularioValidatorStore {
   constructor(protected ctx: HttpContextContract) {}
 
   public schema = schema.create({
-    titulo: schema.string({
-      trim: true,
-    }),
-    tipo: schema.string({
-      trim: true,
-    }),
-    finalidade: schema.string({
-      trim: true,
-    }),
-    // perguntas: schema.object().members({
-      
-    // }),
-    urgencia: schema.number([
-      rules.range(1, 3)]),
+    titulo: schema.string({ trim: true }),
+    tipo: schema.string({ trim: true }),
+    finalidade: schema.string({ trim: true }),
+    perguntas: schema.object().anyMembers(),
+    urgencia: schema.number([rules.range(1, 3)]),
   })
 
   public messages = {
-    required: 'Digite um {{field}}',
+    'required': 'Digite um {{field}}',
     'urgencia.range': 'Insira estrelas de 1 a 3',
-    string: 'O campo {{field}} deve ser uma string',
-    number: 'O campo {{field}} deve ser um inteiro'
+    'string': 'O campo {{field}} deve ser uma string',
+    'number': 'O campo {{field}} deve ser um inteiro',
+  }
+}
+export class FormularioValidatorUpdate {
+  constructor(protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    titulo: schema.string.optional({ trim: true }),
+    tipo: schema.string.optional({ trim: true }),
+    finalidade: schema.string.optional({ trim: true }),
+    perguntas: schema.object.optional().anyMembers(),
+    urgencia: schema.number.optional([rules.range(1, 3)]),
+  })
+
+  public messages = {
+    'urgencia.range': 'Insira estrelas de 1 a 3',
+    'string': 'O campo {{field}} deve ser uma string',
+    'number': 'O campo {{field}} deve ser um inteiro',
   }
 }
