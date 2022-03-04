125ccdf43375535e91c62fa210cb64bfea47fa71
Laura <laurapires@cpejr.com.br>
2022-03-04 14:21:02 -0300

    feat: Validator Mensagem

--- app/Controllers/Http/MensagemsController.ts
+++ app/Controllers/Http/MensagemsController.ts
@@ -3,6 +3,8 @@ import Mensagem from 'App/Models/Mensagem'
 import MensagemsDTO from 'App/DTO/MensagemsDTO'
 import MensagemsRepository from 'App/Repositories/MensagemsRepository'
 import { limpaCamposNulosDeObjeto } from 'App/Utils/Utils'
+import MensagemValidator from 'App/Validators/MensagemValidator'
+import { schema } from '@ioc:Adonis/Core/Validator'
 
 export default class MensagemsController {
   public async index({ request }: HttpContextContract) {
@@ -20,11 +22,13 @@ export default class MensagemsController {
   }
 
   public async store({ request }: HttpContextContract) {
-    const mensagem = request.input('mensagem')
-    const data_envio = request.input('data_envio')
-    const media_url = request.input('media_url')
-    const foi_enviado = request.input('foi_enviado')
-    const foi_visualizado = request.input('foi_visualizado')
+    const validateData = await request.validate(MensagemValidator)
+
+    const mensagem = validateData.mensagem
+    const data_envio = validateData.data_envio
+    const media_url = validateData.media_url
+    const foi_enviado = validateData.foi_enviado
+    const foi_visualizado = validateData.foi_visualizado
     const id_conversa = request.input('id_conversa')
 
     const mensagens = await Mensagem.create({
@@ -42,18 +46,27 @@ export default class MensagemsController {
     const id = request.param('id')
     if (!id) return
 
-    const mensagemData = {
-      id,
-      mensagem: request.input('mensagem'),
-      data_envio: request.input('data_envio'),
-      media_url: request.input('media_url'),
-      foi_enviado: request.input('foi_enviado'),
-      foi_visualizado: request.input('foi_visualizado'),
-      id_conversa: request.input('id_conversa'),
-    } as MensagemsDTO
+    const validatorSchema = schema.create({
+      mensagem: schema.string.optional({ trim: true }),
+      data_envio: schema.date.optional({
+        format: 'dd/MM/yyyy HH:mm:ss',
+      }),
+      media_url: schema.string.optional({ trim: true }),
+      foi_enviado: schema.boolean.optional(),
+      foi_visualizado: schema.boolean.optional(),
+    })
+
+    const validateData = await request.validate({
+      schema: validatorSchema,
+      messages: {
+        'string': 'O campo {{field}} deve ser uma string',
+        'boolean': 'O campo {{field}} deve ser uma boleano',
+        date: 'A data_envio deve ser do formato dd/MM/yyyy HH:mm:ss',
+      },
+    })
 
     const mensagens = await Mensagem.findOrFail(id)
-    mensagens.merge(limpaCamposNulosDeObjeto(mensagemData))
+    mensagens.merge(limpaCamposNulosDeObjeto(validateData))
     await mensagens.save()
 
     return mensagens
