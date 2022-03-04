4f18a2655c552ac89e65b4ccaf5198fc4c9da55b
Laura <laurapires@cpejr.com.br>
2022-03-04 14:46:06 -0300

    feat: Validator Formulario Paciente

---
+++ app/Validators/FormularioPacienteValidator.ts
@@ -0,0 +1,19 @@
+import { schema } from '@ioc:Adonis/Core/Validator'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class FormularioPacienteValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    respostas: schema.string({ trim: true }),
+    midia_url: schema.string.optional({ trim: true }),
+    word: schema.string.optional({ trim: true }),
+    status: schema.boolean(),
+  })
+
+  public messages = {
+    required: 'Digite um {{field}}',
+    string: 'O campo {{field}} deve ser uma string',
+    boolean: 'O campo {{field}} deve ser uma string',
+  }
+}
