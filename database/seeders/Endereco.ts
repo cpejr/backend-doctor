1e1357e4feba48306296dd86acbe174d16373d8e
Maria <mariamsilva15@ufmg.br>
2022-05-31 18:59:10 -0300

    feat: criei consultorios e endereços no seeders

--- database/seeders/Endereco.ts
+++ database/seeders/Endereco.ts
@@ -6,7 +6,7 @@ export default class EnderecoSeeder extends BaseSeeder {
   public async run() {
     await Endereco.createMany([
       {
-        id: "uuid-endereco-1",
+        id: "b94961b7-7bbd-473f-9374-5adae1698d96",
         cep: '30170050',
         pais: 'Brasil',
         estado: 'MG',
@@ -14,6 +14,38 @@ export default class EnderecoSeeder extends BaseSeeder {
         bairro: 'Santo Agostinho',
         rua: 'Rua Matias Cardoso',
         numero: 129,
+        complemento:'Pilotis, sala 01'
+      },
+      {
+        id: "bc40a670-af72-48c0-9678-dbdb0626f74c",
+        cep: '30170050',
+        pais: 'Brasil',
+        estado: 'MG',
+        cidade: 'Belo Horizonte',
+        bairro: 'Santo Agostinho',
+        rua: 'Rua Matias Cardoso',
+        numero: 145,
+        complemento: 'Pilotis, sala 05'
+      },
+      {
+        id: "80673ae3-a55c-46a9-922b-d2652d84b54b",
+        cep: '30130130',
+        pais: 'Brasil',
+        estado: 'MG',
+        cidade: 'Belo Horizonte',
+        bairro: 'Santa Efigênia',
+        rua: 'Rua Rio Grande do Norte',
+        numero: 77
+      },
+      {
+        id: "eb501944-4d84-4766-94b8-2c8e68badb91",
+        cep: '30130130',
+        pais: 'Brasil',
+        estado: 'MG',
+        cidade: 'Belo Horizonte',
+        bairro: 'Santa Efigênia',
+        rua: 'Rua Piauí',
+        numero: 150
       },
     ])
   }
