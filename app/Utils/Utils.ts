764f9fd3cc4e92c806c084494b8ef88325eca780
osbarros <ceobarros@outlook.com>
2022-07-19 15:51:38 -0300

    codigo como nullableAndOptional em usuario Validator

--- app/Utils/Utils.ts
+++ app/Utils/Utils.ts
@@ -1,10 +1,13 @@
 export function limpaCamposNulosDeObjeto(objetoSujo: Object):Object {
+    console.log(objetoSujo);
     const chaves = Object.keys(objetoSujo)
     let objetoLimpo = {}
     chaves.forEach((chave) => {
-      if (objetoSujo[chave] !== undefined && objetoSujo[chave] !== null ) {
+      if (objetoSujo[chave] !== undefined ) {
         objetoLimpo[chave] = objetoSujo[chave]
       }
     })
+
+
     return objetoLimpo
 }
