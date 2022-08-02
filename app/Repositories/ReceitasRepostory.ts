f94cba28fbff9fc0ced1e5e5ae7e9ad26ec6a79c
Laura <laurapires@cpejr.com.br>
2022-08-02 09:13:00 -0300

    feat: nome do usuario na receita

--- app/Repositories/ReceitasRepostory.ts
+++ app/Repositories/ReceitasRepostory.ts
@@ -2,7 +2,12 @@ import Database from '@ioc:Adonis/Lucid/Database'
 import ReceitasDTO from 'App/DTO/ReceitasDTO'
 export default class ReceitasRepository {
   public static async find(params: ReceitasDTO) {
-    const result = await Database.query().from('receitas').where(params)
+    const result = await Database.query()
+      .from('receitas')
+      .where(params)
+      .join('usuarios', 'usuarios.id', '=', 'receitas.id_usuario')
+      .select('receitas.*')
+      .select('usuarios.nome')
 
     return result
   }
