b1d6eb82081fcc82fdef6cc3732be3b53f6520c5
Adrianus Vieira <adrianusvieira@cpejr.com.br>
2022-08-18 11:58:00 -0300

    fix: tentando repassar a chave

--- app/Controllers/Http/ArquivosController.ts
+++ app/Controllers/Http/ArquivosController.ts
@@ -1,39 +1,28 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Arquivo from 'App/Models/Arquivo'
 import Drive from '@ioc:Adonis/Core/Drive'
-import mongoose from 'mongoose'
-const { Schema } = mongoose
 
 export default class ArquivosController {
-  public async index({request}: HttpContextContract) {
-
-  }
+  public async index({ request }: HttpContextContract) {}
 
   public async store({ request, response }: HttpContextContract) {
-    request.multipart
+   request.multipart
       .onFile('imagem', {}, async (file) => {
-        try {
-          const tipo_conteudo = file.headers['content-type']
-          const ACL = 'public-read'
-          const nome = JSON.stringify(file.filename)
-          const chave = `${(Math.random() * 100).toString()}${nome}`
+        const tipo_conteudo = file.headers['content-type']
+        const ACL = 'public-read'
+        const nome = JSON.stringify(file.filename)
+        const chave = `${(Math.random() * 100).toString()}${nome}`
+        const s3file = JSON.stringify(file)
+        await Drive.put(chave, s3file, {
+          tipo_conteudo,
+          ACL,
+        })
 
-          const s3file = JSON.stringify(file)
-
-          const url = await Drive.put(chave, s3file, {
-            tipo_conteudo,
-            ACL,
-          })
-
-          await Arquivo.create({
-            nome,
-            chave,
-            url,
-            tipo_conteudo,
-          })
-        } catch (error) {
-          console.log(error)
-        }
+        await Arquivo.create({
+          nome,
+          chave,
+          tipo_conteudo,
+        })
       })
       .process()
   }
