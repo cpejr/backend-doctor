2219fb1418e9d01519abddfd819e349987c850f7
mariana rabelo <marianarabelo@cpejr.com.br>
2022-12-04 17:28:01 -0300

    fix: numero de letras na string

--- database/migrations/1645242189433_comentarios.ts
+++ database/migrations/1645242189433_comentarios.ts
@@ -6,7 +6,7 @@ export default class Comentarios extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.string('id').primary()
-      table.string('comentario').notNullable()
+      table.string('comentario', 100000).notNullable()
 
       table.timestamp('data_criacao', { useTz: true })
       table.timestamp('data_atualizacao', { useTz: true })
