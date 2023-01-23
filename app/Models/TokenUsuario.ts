6be81b6a86955b481ff0ed5c2e6c33aad701c62e
Lucas Gontijo Carvalho <lucascarvalho@cpejr.com.br>
2023-01-23 18:57:16 -0300

    fix: notificação pronta no backend

--- app/Models/TokenUsuario.ts
+++ app/Models/TokenUsuario.ts
@@ -1,22 +1,13 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column, beforeCreate, belongsTo, BelongsTo } from '@ioc:Adonis/Lucid/Orm'
-import { v4 as uuid } from 'uuid'
+import { BaseModel, column, belongsTo, BelongsTo } from '@ioc:Adonis/Lucid/Orm'
 import Usuario from './Usuario'
 
 export default class TokenUsuario extends BaseModel {
   @column({ isPrimary: true })
-  public id: string
-
-  @beforeCreate()
-  public static async createUUID (model:TokenUsuario){
-    model.id = uuid()
-  }
+  public token_dispositivo: string
 
   @column()
   public id_usuario: string
-
-  @column()
-  public token_dispositivo: string
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
