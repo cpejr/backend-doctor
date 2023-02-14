d034dd08b3a3aa73280913518865441326c4f5c5
lucassoaresilva <lucas.1704@hotmail.com>
2023-02-14 12:16:43 -0300

    feat: criação do controller

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,39 @@
+ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+ import Usuario from 'App/Models/Usuario'
+
+export default class AuthController {
+    async register({request, auth, response}) {
+    
+        let user = await Usuario.create(request.all())
+
+        //generate token for user;
+        let token = await auth.generate(user)
+    
+        Object.assign(user, token)
+    
+        return response.json(user)
+      }
+      async login({request, auth, response}) {
+    
+        let {email, senha} = request.all();
+    
+        try {
+          if (await auth.attempt(email, senha)) {
+            let user = await Usuario.findBy('email', email)
+            let token = await auth.generate(user)
+    
+            Object.assign(user, token)
+            return response.json(user)
+          }
+    
+    
+        }
+        catch (e) {
+          console.log(e)
+          return response.json({message: 'You are not registered!'})
+        }
+      }
+      
+      
+    }
+
