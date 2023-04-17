0ee8691dfdb1620b43110f1d0fb37b5d5884a55b
osbarros <ceobarros@outlook.com>
2023-04-17 10:34:31 -0300

    fix: corrigindo migrations

---
+++ database/migrations/1670885208017_alterables.ts
@@ -0,0 +1,11 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class TokenUsuarios extends BaseSchema {
+  protected tableName = 'alterarables'
+
+  public async up () {
+  }
+
+  public async down () {
+  }
+}
---
+++ database/migrations/1674051694631_alterar_formularios_visualizar_secretarias.ts
@@ -0,0 +1,11 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class TokenUsuarios extends BaseSchema {
+  protected tableName = 'alterar_formularios_visualizar_secretarias'
+
+  public async up () {
+  }
+
+  public async down () {
+  }
+}
